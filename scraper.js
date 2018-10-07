8c8c895e816ca8ef86d5e640bd11fd5124bdd0a2
Matt Lanin <mhlanin@gmail.com>
2018-10-07 09:00:00 -0700

    Collects all shirt data
    Mapped over the urls to collect each page's shirt data, collected data with Promise.all to return it to the next .then() block.

--- scraper.js
+++ scraper.js
@@ -1,8 +1,6 @@
 const scrapeIt = require("scrape-it");
 
 const mainURL = "http://shirts4mike.com/";
-let shirtURLs;
-let shirtData = [];
 
 scrapeIt(`${mainURL}shirts.php`, {
   pages: {
@@ -15,28 +13,28 @@ scrapeIt(`${mainURL}shirts.php`, {
       }
     }
   }
-}).then(({ data, response }) => {
-  console.log(`Status Code For Main Shirts Page: ${response.statusCode}`);
-  // console.log(data.pages);
-  shirtURLs = data.pages.map(a => `${mainURL}${a.url}`);
-  console.log(shirtURLs);
-  scrapeIt(`${shirtURLs[0]}`, {
-    name: {
-      selector: ".shirt-picture img",
-      attr: "alt"
-    },
-    image: {
-      selector: ".shirt-picture img",
-      attr: "src"
-    },
-    price: {
-      selector: "span.price"
-    }
-  }).then(({ data, response }) => {
-    console.log(`Status Code First Shirts Page: ${response.statusCode}`);
-    shirtData.push(data);
-    console.log(shirtData);
+})
+  .then(({ data }) => {
+    const urls = data.pages.map(page => `${mainURL}${page.url}`);
+    console.log(urls);
+    const shirtCalls = urls.map(url =>
+      scrapeIt(url, {
+        name: {
+          selector: ".shirt-picture img",
+          attr: "alt"
+        },
+        image: {
+          selector: ".shirt-picture img",
+          attr: "src"
+        },
+        price: {
+          selector: "span.price"
+        }
+      })
+    );
+    return Promise.all(shirtCalls);
+  })
+  .then(shirtResults => {
+    const shirts = shirtResults.map(shirtResult => shirtResult.data);
+    console.log(shirts);
   });
-  // pageLinks.push(data.pages);
-  // console.log(pageLinks);
-});
